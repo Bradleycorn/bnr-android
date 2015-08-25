9371b4d6dc39528948d82fe04b545b9829137577
Brad Ball <bball@secc.org>
2015-08-25 10:02:39 -0400

    chapter 10 challenge udpates

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,9 +18,11 @@ import java.util.List;
 import java.util.UUID;
 
 public class CrimeListFragment extends Fragment {
+    private static final String TAG = "CrimeListFragment";
 
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mAdapter;
+    private int mAdapterEditIndex = -1;
 
     private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private Crime mCrime;
@@ -47,16 +50,12 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View v) {
+            mAdapterEditIndex = getAdapterPosition();
             Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
-            startActivityForResult(intent, getAdapterPosition());
+            startActivity(intent);
         }
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        mAdapter.notifyItemChanged(requestCode);
-    }
 
     private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
         private List<Crime> mCrimes;
@@ -92,17 +91,50 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-        updateUI();
+        Log.d(TAG, "Crime List Fragment, OnCreateView. Calling updateUI()");
+        //updateUI();
 
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "Crime List Fragment, onResume. Calling updateUI");
+
+        updateUI();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "Crime List Fragment Paused");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "Crime List Fragment Destroyed");
     }
 
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
 
-        mAdapter = new CrimeAdapter(crimes);
-        mCrimeRecyclerView.setAdapter(mAdapter);
+        if (mAdapter == null) {
+            Log.d(TAG, "In updateUI: Adapter is null, creating it now...");
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+        } else {
+            if (mAdapterEditIndex >= 0) {
+                Log.d(TAG, "In updateUI: Adapter exists, and an edit item index is set. Calling adapter.notifyItemChanged");
+                mAdapter.notifyItemChanged(mAdapterEditIndex);
+                mAdapterEditIndex = -1;
+            } else {
+                Log.d(TAG, "In updateUI: Adapter exists, but edit item index is -1. Calling adapter.notifyDataSetChanged");
+                mAdapter.notifyDataSetChanged();
+            }
+        }
 
     }
 }
